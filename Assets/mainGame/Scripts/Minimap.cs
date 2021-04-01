7556f2a4706e925c40a814c924521eba7ed8e24d
hannahbish <510894@chrome.escg.ac.uk>
2021-04-01 14:23:37 +0100

    mini map

---
+++ Assets/mainGame/Scripts/Minimap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+
+    public Transform player;
+
+    void LateUpdate()
+    {
+        player = FindObjectOfType<Player>().gameObject.transform;
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        newPosition.z = -10;
+        transform.position = newPosition;
+
+        //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+
+}
