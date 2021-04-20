e833a2872bd2e79c1a375709068a34dd540b1232
hannahbish <510894@chrome.escg.ac.uk>
2021-04-20 11:56:20 +0100

    editing

--- Assets/mainGame/Scripts/PlayerDeath.cs
+++ Assets/mainGame/Scripts/PlayerDeath.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-    public GameObject enemy;
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
